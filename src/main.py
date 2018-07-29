d0ee534385c275e342e9f77e2f8480171b1a1ad1
dsaunders45459 <dsaunders45459@gmail.com>
2018-07-29 14:42:33 -0700

    gitignore

--- src/main.py
+++ src/main.py
@@ -1,4 +1,35 @@
 import json
+import camelcase
+import mymodule
+
+x = mymodule.greeting("Jonathan")
+print(len(x))
+
+b = "Hello, World!"
+print(b[2:4])
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+cars = ["Ford", "Volvo", "BMW"]
+cars.sort()
+print(cars[-1])
+
+
+def x(a, b): return a * b
+
+
+b = x(1, 2)
+print(b)
+
+print(x(5, 6))
+x = "test"
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))
 
 
 class Test:
@@ -13,7 +44,7 @@ print p1.name
 
 x = '{ "name":"John", "age":30, "city":"New York"}'
 y = json.loads(x)
-print(y.age)
+print(y["age"])
 
 thistuple = ("apple", "banana", "cherry")
 for val in thistuple:
